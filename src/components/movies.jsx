f088953b86776ac31271e9cef4d56e017a00baa6
Yanjun7 <yjzhou@gwmail.gwu.edu>
2020-08-31 15:20:20 +0800

    Extract method and Destructure arguments.

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -46,8 +46,7 @@ class Movies extends Component {
     this.setState({ sortColumn });
   };
 
-  render() {
-    const { length: count } = this.state.movies;
+  getPageData = () => {
     const {
       pageSize,
       currentPage,
@@ -55,8 +54,6 @@ class Movies extends Component {
       sortColumn,
       movies: allMovioes,
     } = this.state;
-
-    if (count === 0) return <p>There are no movies in database.</p>;
     const filtered =
       selectedGenre && selectedGenre._id
         ? allMovioes.filter((m) => m.genre._id === selectedGenre._id)
@@ -64,7 +61,16 @@ class Movies extends Component {
     const sorted = _.sortBy(filtered, [sortColumn.path], [sortColumn.order]);
 
     const movies = paginate(sorted, currentPage, pageSize);
+    return { totalCount: filtered.length, data: movies };
+  };
 
+  render() {
+    const { length: count } = this.state.movies;
+    const { pageSize, currentPage, sortColumn } = this.state;
+
+    if (count === 0) return <p>There are no movies in database.</p>;
+
+    const { totalCount, data: movies } = this.getPageData();
     return (
       <div className="row">
         <div className="col-3">
@@ -75,7 +81,7 @@ class Movies extends Component {
           />
         </div>
         <div className="col">
-          <p>Showing {filtered.length} movies in the database.</p>
+          <p>Showing {totalCount} movies in the database.</p>
           <MoviesTable
             movies={movies}
             sortColumn={sortColumn}
@@ -84,7 +90,7 @@ class Movies extends Component {
             onSort={this.handleSort}
           />
           <Pagination
-            itemsCount={filtered.length}
+            itemsCount={totalCount}
             pageSize={pageSize}
             currentPage={currentPage}
             onPageChange={this.handlePageChange}

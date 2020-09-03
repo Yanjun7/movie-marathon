2989410e0aa8c93d1237bcf6f01a5075b9842429
Yanjun7 <yjzhou@gwmail.gwu.edu>
2020-09-03 20:42:45 +0800

    Register routing to App.js

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
 import React, { Component } from "react";
 import "./App.css";
+import { Route, Redirect, Switch } from "react-router-dom";
 import Movies from "./components/movies";
+
 import "bootstrap/dist/css/bootstrap.css";
 import "font-awesome/css/font-awesome.css";
+import Customers from "./components/customers";
+import Rentals from "./components/rentals";
+import NotFound from "./components/notFound";
 
 class App extends Component {
-  state = {};
   render() {
     return (
       <main className="container">
-        <Movies />
+        <Switch>
+          <Route path="/movies" component={Movies}></Route>
+          <Route path="/customers" component={Customers}></Route>
+          <Route path="/rentals" component={Rentals}></Route>
+          <Route path="/not-found" component={NotFound}></Route>
+          <Redirect from="/" exact to="/movies" />
+          <Redirect to="/not-found" />
+        </Switch>
       </main>
     );
   }
---
+++ src/components/movieForm.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MovieFroms = () => {
+  return <h1>MovieFroms</h1>;
+};
+
+export default MovieFroms;
